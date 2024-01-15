36a81c11308cdf8a5d8c63a85fbc4e81746f08e7
unknown <64825962+D1urga@users.noreply.github.com>
2024-01-15 23:32:17 +0530

    files changed

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -4,41 +4,44 @@ import { FaFacebook, FaGoogle, FaInstagram, FaTwitter } from "react-icons/fa";
 
 export default function Footer() {
   return (
-    <div className={styles.outer_div}>
-      <div className={styles.div1}>
-        <p className={styles.heading}>edCom</p>
-        <p className={styles.content}>
-          Flutter transforms the development process. Build, test, and deploy
-          beautiful mobile, web, desktop, and embedded experiences from a single
-          codebase.
-        </p>
-      </div>
-      <div className={styles.div2}>
-        <h4>Navigation</h4>
-        <p>Leetcode</p>
-        <p>Flutter</p>
-        <p>Next Js</p>
-        <p>Express</p>
-      </div>
-      <div className={styles.div3}>
-        <h4>More</h4>
-        <p>Node Js</p>
-        <p>React Js</p>
-        <p>Competitive</p>
-        <p>more...</p>
-      </div>
-      <div className={styles.div4}>
-        <h4>Contact US</h4>
-        <p>Anoop Chaudhary</p>
-        <p>Anup@gmail.com</p>
-        <p>+91-6396937375</p>
-        <div className={styles.icons}>
-          <FaGoogle className={styles.logo} />
-          <FaInstagram className={styles.logo} />
-          <FaFacebook className={styles.logo} />
-          <FaTwitter className={styles.logo} />
+    <>
+      <div className={styles.outer_div}>
+        <div className={styles.div1}>
+          <p className={styles.heading}>edCom</p>
+          <p className={styles.content}>
+            Flutter transforms the development process. Build, test, and deploy
+            beautiful mobile, web, desktop, and embedded experiences from a
+            single codebase.
+          </p>
+        </div>
+        <div className={styles.div2}>
+          <h4>Navigation</h4>
+          <p>Leetcode</p>
+          <p>Flutter</p>
+          <p>Next Js</p>
+          <p>Express</p>
+        </div>
+        <div className={styles.div3}>
+          <h4>More</h4>
+          <p>Node Js</p>
+          <p>React Js</p>
+          <p>Competitive</p>
+          <p>more...</p>
+        </div>
+        <div className={styles.div4}>
+          <h4>Contact US</h4>
+          <p>Anoop Chaudhary</p>
+          <p>Anup@gmail.com</p>
+          <p>+91-6396937375</p>
+          <div className={styles.icons}>
+            <FaGoogle className={styles.logo} />
+            <FaInstagram className={styles.logo} />
+            <FaFacebook className={styles.logo} />
+            <FaTwitter className={styles.logo} />
+          </div>
         </div>
       </div>
-    </div>
+      <p className={styles.intro}>Developed by Anoop Chaudhary</p>
+    </>
   );
 }
