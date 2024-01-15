bcf91a8829b7a6e5e6bbbec4de565b50442ad167
unknown <64825962+D1urga@users.noreply.github.com>
2024-01-15 17:09:46 +0530

    files changed

---
+++ src/app/components/navbar.js
@@ -0,0 +1,90 @@
+"use client";
+import React from "react";
+import styles from "./styles/Navbar.module.css";
+import { useState } from "react";
+import {
+  FaBars,
+  FaBath,
+  FaBeer,
+  FaFacebook,
+  FaGithub,
+  FaGoogle,
+  FaInstagram,
+  FaLinkedin,
+  FaSnowflake,
+  FaTwitter,
+  FaXbox,
+} from "react-icons/fa";
+import Link from "next/link";
+
+export default function Navbar() {
+  const [isopen, setIsopen] = useState(false);
+  return (
+    <div className={styles.outer_div}>
+      <div className={styles.icon}>
+        <FaSnowflake className={styles.logo} />
+        <p>edCom</p>
+      </div>
+      <ul className={isopen ? `${styles.ul_list}` : `${styles.ul_list1}`}>
+        <li>
+          <Link
+            href="/"
+            className={styles.link}
+            onClick={() => {
+              setIsopen(!isopen);
+            }}
+          >
+            Leetcode
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/"
+            className={styles.link}
+            onClick={() => {
+              setIsopen(!isopen);
+            }}
+          >
+            Flutter
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/"
+            className={styles.link}
+            onClick={() => {
+              setIsopen(!isopen);
+            }}
+          >
+            Next Js
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/"
+            className={styles.link}
+            onClick={() => {
+              setIsopen(!isopen);
+            }}
+          >
+            Express
+          </Link>
+        </li>
+      </ul>
+      <div className={styles.icon_list}>
+        <FaGoogle className={styles.logos} />
+        <FaInstagram className={styles.logos} />
+        <FaFacebook className={styles.logos} />
+        <FaTwitter className={styles.logos} />
+        <FaLinkedin className={styles.logos} />
+        <FaGithub className={styles.logos} />
+      </div>
+      <FaBars
+        className={styles.menu}
+        onClick={() => {
+          setIsopen(!isopen);
+        }}
+      />
+    </div>
+  );
+}
